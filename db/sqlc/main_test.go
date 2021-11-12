31054ea4f1ca27c01a842424f06a5a3d3513a687
shalahuddinn <shalahuddinnn@yahoo.com>
2021-11-12 09:40:21 +0700

    handle concurrency db

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Can not connect to DB:", err)
+	}
+	testQueries = New(testDB)
+	os.Exit(m.Run())
+}
---
+++ db/sqlc/store.go
@@ -0,0 +1,221 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// type Store interface {
+// 	// Querier
+// 	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+// }
+
+type Store struct {
+	db *sql.DB
+	*Queries
+}
+
+// NewStore creates a new store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// func NewStore(db *sql.DB) *Store {
+// 	return &Store{
+// 		db:      db,
+// 		Queries: New(db),
+// 	}
+// }
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+var txKey = struct{}{}
+
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		txName := ctx.Value(txKey)
+
+		fmt.Println(txName, "create transfer")
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(txName, "create entry 1")
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(txName, "create entry 2")
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO: update accounts' balance
+		if arg.FromAccountID < arg.ToAccountID {
+			// // move money out of account1
+			// result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			// 	ID:     arg.FromAccountID,
+			// 	Amount: -arg.Amount,
+			// })
+			// if err != nil {
+			// 	return err
+			// }
+
+			// // move money into account2
+			// result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			// 	ID:     arg.ToAccountID,
+			// 	Amount: arg.Amount,
+			// })
+			// if err != nil {
+			// 	return err
+			// }
+
+			// move money out of account1
+			fmt.Println(txName, "get account 1")
+			account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
+			if err != nil {
+				return err
+			}
+
+			if account1.Balance < arg.Amount {
+				err = fmt.Errorf("%v: not sufficient balance", account1.ID)
+				return err
+			}
+
+			fmt.Println(txName, "update account 1")
+			result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+				ID:      arg.FromAccountID,
+				Balance: account1.Balance - arg.Amount,
+			})
+			if err != nil {
+				return err
+			}
+
+			// move money into account2
+			fmt.Println(txName, "get account 2")
+			account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(txName, "update account 2")
+			result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+				ID:      arg.ToAccountID,
+				Balance: account2.Balance + arg.Amount,
+			})
+			if err != nil {
+				return err
+			}
+		} else {
+			// // move money into account2
+			// result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			// 	ID:     arg.ToAccountID,
+			// 	Amount: arg.Amount,
+			// })
+			// if err != nil {
+			// 	return err
+			// }
+
+			// // move money out of account1
+			// result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			// 	ID:     arg.FromAccountID,
+			// 	Amount: -arg.Amount,
+			// })
+			// if err != nil {
+			// 	return err
+			// }
+
+			// move money into account2
+			fmt.Println(txName, "get account 2")
+			account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(txName, "update account 2")
+			result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+				ID:      arg.ToAccountID,
+				Balance: account2.Balance + arg.Amount,
+			})
+			if err != nil {
+				return err
+			}
+
+			// move money out of account1
+			fmt.Println(txName, "get account 1")
+			account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
+			if err != nil {
+				return err
+			}
+
+			if account2.Balance < arg.Amount {
+				err = fmt.Errorf("%v: not sufficient balance", account2.ID)
+				return err
+			}
+
+			fmt.Println(txName, "update account 1")
+			result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+				ID:      arg.FromAccountID,
+				Balance: account1.Balance - arg.Amount,
+			})
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	return result, err
+}
